8c3bcfe9e292e56a1ad93d1c8cae09fbc9ca5b59
Igor Kenzo <36746677+IgorKenzo@users.noreply.github.com>
2018-09-04 10:28:45 -0300

    CriacaoDoProjeto
    Criação do Zero04/09

---
+++ app/src/main/java/com/example/a3aetim/prototipomobilebdinterno/Splash.java
@@ -0,0 +1,32 @@
+package com.example.a3aetim.prototipomobilebdinterno;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        final Intent intent = new Intent(this, MainActivity.class);
+        Thread thread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }; thread.start();
+    }
+}
