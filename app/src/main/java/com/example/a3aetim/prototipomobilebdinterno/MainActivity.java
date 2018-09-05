85f314c2117f420c2782e7346b612d952752b68c
JapaNegro <36746677+JapaNegro@users.noreply.github.com>
2018-09-05 18:48:19 -0300

    Mudado a cor dos itens do menu e os textos
    05/09

--- app/src/main/java/com/example/a3aetim/prototipomobilebdinterno/MainActivity.java
+++ app/src/main/java/com/example/a3aetim/prototipomobilebdinterno/MainActivity.java
@@ -71,17 +71,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_Search) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_Market) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_Avaliation) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_Discussions) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_Confing) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_RateUs) {
 
         }
 
